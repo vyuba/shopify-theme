a1964213aeb1acf6097f380e2d1e24ccdb744f68
vyuba <preyealexander731@gmail.com>
2024-05-21 19:23:35 +0100

    created a the homepage

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,17 @@
 // Put your application javascript here
+    let dropdownBtn = document.querySelectorAll(".dropdownBtn")
+    let list = document.querySelectorAll(".list")
+    console.log(list)
+
+    dropdownBtn.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            // Toggle the display of the corresponding list
+            list.forEach((list, liIndex) => {
+                if (liIndex === index) {
+                    list.style.display = list.style.display === "block" ? "none" : "block";
+                } else {
+                    list.style.display = "none"; // Optionally hide other lists
+                }
+            });
+        });
+    });
